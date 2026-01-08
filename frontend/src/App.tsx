7470c4228cc0bc975076b65f8a82ed8684fa17b0
mzabron <mzabron@student.agh.edu.pl>
2026-01-08 20:52:53 +0100

    set up react ts toolchain

---
+++ frontend/src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+    return( 
+    <>
+        <h1>React App</h1>
+        <h2>Administrator Panel</h2>
+    </>
+    );
+}
---
+++ frontend/src/index.tsx
@@ -0,0 +1,12 @@
+import {createRoot} from 'react-dom/client';
+import App from './App';
+import { StrictMode } from 'react';
+
+const domNode = document.getElementById('root') as HTMLElement;
+const root = createRoot(domNode);
+
+root.render(
+    <StrictMode>
+        <App />
+    </StrictMode>
+);
